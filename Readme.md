0d7f6b92f4c4009d8f21199cd29379384da7ccf0
emo-mruczek <krokcia1@gmail.com>
2026-09-09 15:49:49 +0200

    fix gitignore

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# CIAPEK
+## 16-bit CPU, MIPS-like
+### made in VHDL for Mimas V2 FPGA
+#### with help of Digital Design and Computer Architecture book
+
+##### Part 0 - introduction
+
+Why 16-bit? Because everyone else does 8 or 32 or 64.
+
+##### Part 1 - can i even flash my FPGA on NixOs?
+
+##### Part 2 - constructing an ISA for 16-bit datapath
+
+##### Part 3 - implementing state elements
+
+##### TODO
+
+- [ ] debouncing pushbutton 
+- [ ] making normaln gitignore
+- [ ] this README
+- [ ] formatter
---
+++ ciapek/src/register_file.vhd
@@ -0,0 +1,128 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL; 
+
+-- two read ports (ra1, ra2) and one write port (ra3)
+-- each specifies one of the 7 registers
+entity register_file is
+    Port ( clk : in  STD_LOGIC;
+           we3_in : in  STD_LOGIC; -- write enable
+	   ra1_in : in  STD_LOGIC_VECTOR (2 downto 0);
+           ra2_in : in  STD_LOGIC_VECTOR (2 downto 0);
+           ra3_in : in  STD_LOGIC_VECTOR (2 downto 0);
+           wd3_in : in  STD_LOGIC_VECTOR (15 downto 0); -- write data
+           rd1_out : out  STD_LOGIC_VECTOR (15 downto 0);
+           rd2_out : out  STD_LOGIC_VECTOR (15 downto 0);
+           -- for debugging on MIMASv2
+           dip0, dip1, dip2, dip3, dip4, dip5, dip6, dip7 : in STD_LOGIC := '0';
+           led0, led1, led2, led3, led4, led5, led6, led7 : out STD_LOGIC;
+           button0 : in STD_LOGIC);
+end register_file;
+
+architecture Behavioral of register_file is
+
+-- register/ram
+type registers_type is array (15 downto 0) of STD_LOGIC_VECTOR (15 downto 0);
+signal registers: registers_type; -- internal var
+
+-- for state machine for displaying register content
+type statetype is (L, H);
+signal disp_state, disp_nextstate: statetype;
+
+begin
+
+-- writting on rising edge
+-- in processes signals keep theri old value
+-- until an event in the sens. list takes place
+process(clk) begin 
+if clk'event and clk = '1' then -- RISING_EDGE(clk)
+	if we3_in = '1' then
+		registers(CONV_INTEGER(ra3_in)) <= wd3_in;
+	end if;
+end if;
+end process;
+
+-- reading 
+-- clock doesnt matter
+process(ra1_in, ra2_in) begin
+	-- reg 0 always 0
+	if (CONV_INTEGER(ra1_in) = 0) then
+		rd1_out <= (others => '0');
+	else
+		rd1_out <= registers(CONV_INTEGER(ra1_in));
+	end if;
+
+	-- for both ports
+	if (CONV_INTEGER(ra2_in) = 0) then
+		rd2_out <= (others => '0');
+	else
+		rd2_out <= registers(CONV_INTEGER(ra2_in));
+	end if;
+end process;
+
+-- state machine 
+-- https://stackoverflow.com/questions/7589443/xilinx-error-place-1018-message
+-- TODO: fix, its very silly and not working properly
+process(clk, button0) begin
+
+  --  pull up
+  if button0 = '0' and rising_edge(clk) then 
+    disp_state <= disp_nextstate;
+  end if;
+
+end process;
+
+disp_nextstate <= L when disp_state = H else H;
+
+
+process(dip0, dip1, dip2, dip3, dip4, dip5, dip6, dip7)
+  -- variables can only be used inside processes 
+  variable registers_choice : STD_LOGIC_VECTOR(7 downto 0);
+  variable register_choosen : STD_LOGIC_VECTOR(15 downto 0);
+begin
+  registers_choice := dip0 & dip1& dip2 & dip3 & dip4 & dip5 & dip6 & dip7;
+
+  -- DEBUG 
+  -- registers(1) <= "0111000000000111";
+  -- registers(0) <= "0100000000000100";
+  
+  -- for now disp only lower bits
+  -- 0 when ON
+  case registers_choice is 
+    when "11111110" => register_choosen := registers(0);
+    when "11111101" => register_choosen := registers(1);   
+    when "11111011" => register_choosen := registers(2);
+    when "11110111" => register_choosen := registers(3);
+    when "11101111" => register_choosen := registers(4);
+    when "11011111" => register_choosen := registers(5);
+    when "10111111" => register_choosen := registers(6);
+    when "01111111" => register_choosen := registers(7);
+    when others => register_choosen := registers(0);
+  end case;
+
+  -- disp lb 
+  
+  if disp_state = L then 
+     led0 <= register_choosen(7);
+     led1 <= register_choosen(6);
+     led2 <= register_choosen(5);
+     led3 <= register_choosen(4);
+     led4 <= register_choosen(3);
+     led5 <= register_choosen(2);
+     led6 <= register_choosen(1);
+     led7 <= register_choosen(0);
+  else 
+    led0 <= register_choosen(15);
+    led1 <= register_choosen(14);
+    led2 <= register_choosen(13);
+    led3 <= register_choosen(12);
+    led4 <= register_choosen(11);
+    led5 <= register_choosen(10);
+    led6 <= register_choosen(9);
+    led7 <= register_choosen(8);
+  end if;
+
+end process;
+
+end Behavioral;
+
