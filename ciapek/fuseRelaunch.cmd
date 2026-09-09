dc1e8bbf2302a959411951522345755497a8027f
emo-mruczek <krokcia1@gmail.com>
2026-09-09 15:39:21 +0200

    drop from gitignore

--- ciapek/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/felix/prjcts/ciapek/ciapek/register_file_isim_beh.exe" -prj "/home/felix/prjcts/ciapek/ciapek/register_file_beh.prj" "work.register_file" 
